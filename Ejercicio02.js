26cbd2965f35cd497f5030c259e6c24117da67a4
Farkonetti <115066091+Farkonetti@users.noreply.github.com>
2022-10-14 21:10:42 +0200

    Add files via upload

---
+++ Ejercicio02.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Francsico",
+    29,
+    true,
+    new Date(1993, 1, 29),
+    {
+        titulo: "The End of All Evil",
+        autor: "Jeremy Locke",
+        fecha: new Date(2006, 2, 1),
+        url: "https://www.amazon.com/End-All-Evil-Jeremy-Locke/dp/0977745104"
+    }
+];
+
