56d3dc6eeb8ea22d1223b9c866ab78ffc534a82b
Farkonetti <115066091+Farkonetti@users.noreply.github.com>
2022-10-16 21:03:13 +0200

    Add files via upload

---
+++ fechas.js
@@ -0,0 +1,12 @@
+const fecha = new Date();
+
+const birthDate = new Date(1993, 0, 29);
+
+const mira = fecha > birthDate;
+
+const diaNacimiento = birthDate.getDate();
+
+const mesNacimiento = birthDate.getMonth() + 1;
+
+const añoNacimiento = birthDate.getFullYear();
+    
