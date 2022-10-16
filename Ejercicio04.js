ce2e2b9e2c065fd6761d9034284dda9399e8a9aa
Farkonetti <115066091+Farkonetti@users.noreply.github.com>
2022-10-16 12:46:00 +0200

    Add files via upload

---
+++ Ejercicio04.js
@@ -0,0 +1,10 @@
+const name = "Francisco";
+const lastName = "Paneque";
+const estudiante = name.concat(" ").concat(lastName);
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const estudianteLength = estudiante.length();
+const inicialNombre = name.substring(0, 1)
+const finalApellido = lastname.substring(lastName.length - 1, lastName.length);
+const borrarEspacios = estudiante.replace(/ /g, "");
+const verdadName = estudiante.includes(name);
